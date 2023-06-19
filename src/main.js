ca13814f4937a7cd2fc7e7ec5e19caf911e6cb18
valentino bonavento <valentino.bonavento@gmail.com>
2023-06-19 18:01:32 -0300

    express

--- src/main.js
+++ src/main.js
@@ -45,3 +45,32 @@ try {
     console.log(error.message)
 }
 
+const app = express()
+
+app.get('/products', (req, res) => {
+    const limit = req.query.limit
+     fs.readFile('./database/products.json', 'utf-8', (err, info) => {
+        if (err) throw err
+    let products = JSON.parse(info)
+    if(limit){
+        products = products.slice(0, limit)
+    }
+    res.json(products)
+})
+})
+
+app.get('/products/:id', (req, res) => {
+    const id = req.params.id
+     fs.readFile('./database/products.json', 'utf-8', (err, info) => {
+        if (err) throw err
+        let products = JSON.parse(info)
+        const product = products.find(p => p.id.toString() === id.toString())
+        if (product){
+            res.json(product)
+        } else {
+            res.status(404).send('No existe el producto con el ID solicitado')
+        }
+    })
+})
+
+const server = app.listen(8080)
